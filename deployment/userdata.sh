e6868632baec8ae05a248e025adbbf5919c54686
DikshaPachpande <dikshapachpande@gmail.com>
2026-07-03 18:58:38 +0530

    Update userdata.sh

--- deployment/userdata.sh
+++ deployment/userdata.sh
@@ -1,1 +1,15 @@
+#!/bin/bash
 
+yum update -y
+
+yum install python3 git nginx -y
+
+pip3 install flask
+
+pip3 install flask-cors
+
+pip3 install pymysql
+
+systemctl enable nginx
+
+systemctl start nginx
