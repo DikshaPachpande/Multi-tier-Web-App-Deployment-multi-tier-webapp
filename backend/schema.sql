771e887cb9326123a107558c5a4873b61eb062fd
DikshaPachpande <dikshapachpande@gmail.com>
2026-07-03 18:58:15 +0530

    Update schema.sql

--- backend/schema.sql
+++ backend/schema.sql
@@ -1,1 +1,18 @@
+CREATE DATABASE company;
 
+USE company;
+
+CREATE TABLE users(
+
+id INT AUTO_INCREMENT PRIMARY KEY,
+
+name VARCHAR(50)
+
+);
+
+INSERT INTO users(name)
+VALUES
+('John'),
+('Alice'),
+('David'),
+('Maria');
